e8be5c665a666ef66573d12bf2f1da456d34afba
Masud Parvez <masud.parvez@brainstation.com>
2024-05-09 18:01:35 +0600

    Bootswatch insert

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> item = _db.Categories.ToList();
+            return View(item);
+        }
+    }
+}
